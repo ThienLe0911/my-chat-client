8471f5711e00d06718f663c710f1e35e5b770b5d
thienlh <thienlh@hexagon.xyz>
2025-05-04 22:55:05 +0700

    socket config

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://my-chat-server-59hl.onrender.com/'); // Đảm bảo đúng cổng backend bạn đang chạy
+const socket = io('https://my-chat-server-59hl.onrender.com'); // Đảm bảo đúng cổng backend bạn đang chạy
 
 export default socket;
